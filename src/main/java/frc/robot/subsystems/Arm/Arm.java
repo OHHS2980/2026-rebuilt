207650d7bff27e9caac82048451c1a86d8d3dd2c
abcde986 <lixinelin@gmail.com>
2025-12-22 00:47:31 -0800

    u

--- src/main/java/frc/robot/subsystems/Arm/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm/Arm.java
@@ -3,5 +3,43 @@ package frc.robot.subsystems.Arm;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
+        
+    ArmIO armIO;
 
+    public Arm(ArmIO armIO)
+    {
+        this.armIO = armIO;
+    }
+
+    class moveArm90degreesyay extends Command
+    {
+        public void initialize()
+        {
+            armIO.setSetPoint(90);
+        }
+
+        public void execute()
+        {
+            armIO.update();
+        }
+
+        public boolean isFinished()
+        {
+            if (Math.abs(armIO.getAngleFromPosition() - 90) < 2.5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void end(boolean isFinished)
+        {
+            armIO.setMotor(0);
+        }
+    }
+
+    public void periodic()
+    {
+        
+    }
 }
